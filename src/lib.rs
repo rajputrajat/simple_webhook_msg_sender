02aa43554b8730ee949c54998211f3af2fe3c482
Rajat Rajput <rajputrajat@gmail.com>
2021-10-23 15:29:21 +0530

    rearrange code block

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,25 @@
 use reqwest::{Client, Error as ReqError, Response};
 use serde::Serialize;
 
+pub struct WebhookSender {
+    url: String,
+    web_client: Client,
+}
+
+impl WebhookSender {
+    pub fn new<S: Into<String>>(url: S) -> Self {
+        Self {
+            url: url.into(),
+            web_client: Client::new(),
+        }
+    }
+
+    pub async fn post(&self, message: &str) -> Result<Response, ReqError> {
+        let msg = get_sample_msg(message);
+        self.web_client.post(&self.url).json(&msg).send().await
+    }
+}
+
 fn get_sample_msg(message: &str) -> JsonMessage {
     let mut msg = get_msg_template();
     msg.attachments
@@ -35,25 +54,6 @@ const fn get_msg_template() -> JsonMessage {
                 },
             }
         }],
-    }
-}
-
-pub struct WebhookSender {
-    url: String,
-    web_client: Client,
-}
-
-impl WebhookSender {
-    pub fn new<S: Into<String>>(url: S) -> Self {
-        Self {
-            url: url.into(),
-            web_client: Client::new(),
-        }
-    }
-
-    pub async fn post(&self, message: &str) -> Result<Response, ReqError> {
-        let msg = get_sample_msg(message);
-        self.web_client.post(&self.url).json(&msg).send().await
     }
 }
 
